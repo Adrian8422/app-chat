e08a2bd0b488770599330fa84e7aaf87370acd93
Adrian Villegas <manbassman1996@gmail.com>
2023-03-10 14:44:43 -0300

    change-db

--- db.ts
+++ db.ts
@@ -1,14 +1,13 @@
-import * as admin from"firebase-admin"
-const serviceAccount = require("./key.json")
-
+import * as admin from "firebase-admin";
+const serviceAccount = require("./key.json");
 
 admin.initializeApp({
-  credential:admin.credential.cert(serviceAccount as any),
-  databaseURL :"https://apx-dwf-m6-a6750-default-rtdb.firebaseio.com"
-})
-const firestore = admin.firestore()
-firestore.settings({ignoreUndefinedProperties:true})
+  credential: admin.credential.cert(serviceAccount as any),
+  databaseURL: "https://prueba-deploy-m8-default-rtdb.firebaseio.com",
+});
+const firestore = admin.firestore();
+firestore.settings({ ignoreUndefinedProperties: true });
 
-const rtdb = admin.database()
+const rtdb = admin.database();
 
-export {firestore,rtdb}
+export { firestore, rtdb };
